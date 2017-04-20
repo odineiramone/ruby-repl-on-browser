51fc3b12c2bf03dddaebd1b9ea11634a7fc78da5
Odinei Rocha <odinei.ribeiro@live.com>
2017-04-20 18:34:02 -0300

    word warper and some css

--- js/default.js
+++ js/default.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
   var code = $(".codemirror-textarea")[0];
   var editor = CodeMirror.fromTextArea(code, {
     lineNumbers : true,
+    lineWrapping : true,
     mode : "ruby",
     theme : "monokai"
   });
