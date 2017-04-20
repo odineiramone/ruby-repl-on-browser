96a8f842a8ad47cc83bf967c6542df14cc7e7c76
Odinei Rocha <odinei.ribeiro@live.com>
2017-04-20 18:06:15 -0300

    codemirror working

---
+++ js/default.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+  var code = $(".codemirror-textarea")[0];
+  var editor = CodeMirror.fromTextArea(code, {
+    lineNumbers : true,
+    mode : "ruby",
+    theme : "monokai"
+  });
+});
