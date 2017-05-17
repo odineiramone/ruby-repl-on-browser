27bff5522440192a7604eda25aeac3e1f070ef98
Odinei Rocha <odinei.ribeiro@live.com>
2017-05-17 12:00:06 -0300

    post with html

--- application.rb
+++ application.rb
@@ -10,8 +10,10 @@ end
 namespace '/api' do
   post '/ruby' do
     result = eval("begin $stdout = StringIO.new;
-                  #{params[:cmd]}; $stdout.string;
+                  #{params[:code]}; $stdout.string;
                   ensure $stdout = STDOUT end")
+
+    result += "\n=>" + eval(params[:code]).to_s
 
     payload = { result: result }
     return payload.to_json
