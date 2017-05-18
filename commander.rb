525152ae3c009ec65bbc50bda4eaac416017cf48
Odinei Rocha <odinei.ribeiro@live.com>
2017-05-18 12:38:24 -0300

    more protection

--- commander.rb
+++ commander.rb
@@ -1,14 +1,22 @@
 # rubocop:disable Security/Eval
 # rubocop:disable Style/Documentation
+class ForbiddenMethodError < StandardError
+  def message
+    "Please, don't use this method!"
+  end
+end
+
 class Commander
   def execute(command)
+    raise ForbiddenMethodError if command =~ /eval( |\()/
+
     result = eval("begin $stdout = StringIO.new;
                   #{command}; $stdout.string;
                   ensure $stdout = STDOUT end")
 
     result += '=> ' + (eval(command) || 'nil').to_s
     result
-  rescue StandardError => e
-    e.to_s
+  rescue => e
+    "#{e.class}: #{e.message}"
   end
 end
